479304f6211c6c65946835749019707a51de75a4
TizianoPitzalis <125460386+TizianoPitzalis@users.noreply.github.com>
2026-02-26 18:03:32 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+
+import pygame
+from source.game import Game
+from source.menu import Menu
+
+def main():
+    menu = Menu()
+    data = menu.run()
+    if not(data["load_dungeon"]):
+        return
+
+    while data != None:
+        pygame.init()
+        game = Game(id=data["id"], save=data["save"], entrance=data["entrance"])
+        data = game.run()
+
+main()
